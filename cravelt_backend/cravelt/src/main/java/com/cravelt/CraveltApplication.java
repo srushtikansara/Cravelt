a09b8be8f0f1e11c7b8104784aea4056fcf62fc2
Srushti Kansara <srushtikansara06@gmail.com>
2026-04-22 12:41:48 +0530

    Fix: In craveltapplication

--- cravelt_backend/cravelt/src/main/java/com/cravelt/CraveltApplication.java
+++ cravelt_backend/cravelt/src/main/java/com/cravelt/CraveltApplication.java
@@ -1,13 +1,24 @@
 package com.cravelt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CraveltApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CraveltApplication.class, args);
-	}
+    @Value("${spring.data.mongodb.uri}")  // ← add this
+    private String mongoUri;              // ← add this
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraveltApplication.class, args);
+    }
+
+    @PostConstruct                        // ← add this whole method
+    public void logMongoUri() {
+        System.out.println(">>> MONGO URI = " + mongoUri);
+    }
 
 }
